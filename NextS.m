520386e7d38f10dc08b336faccc6b3820edc201a
Sarnlest <44273126+Sarnlest@users.noreply.github.com>
2018-10-18 13:01:07 -0700

    Add files via upload
    Added more comments

--- NextS.m
+++ NextS.m
@@ -1,14 +1,15 @@
 function [SPrime] = NextS(pe,s,a)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-b=rand();
-for i=0:5;
-    for j=0:5;
-        for k=0:11;
-            b=b-TransProb2(pe,s,a,[i,j,k]);
+%Generates the next state given an error probability Pe, current state S,
+%and action a
+%   Uses 
+b=rand(); %Generate a number between 0 and 1
+for i=0:5; %x'
+    for j=0:5; %y'
+        for k=0:11; %h'
+            b=b-TransProb2(pe,s,a,[i,j,k]); %Subtract the transition probability from the number
             if b<=0
-                SPrime=[i,j,k];
-                b=b+100;
+                SPrime=[i,j,k]; %If the number goes negative, the checked next state is the actual next state
+                b=b+100; %prevent the number from going negative again, ensures only the one action was taken.
             end
         end
     end
